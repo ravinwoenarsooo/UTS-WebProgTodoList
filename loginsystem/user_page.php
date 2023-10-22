4f2aef692b7971f1db2e89b7a8e262d72af31aa4
khalebandhyka <khaleb.andhyka@gmail.com>
2023-10-22 20:06:43 +0700

    dor

--- loginsystem/user_page.php
+++ loginsystem/user_page.php
@@ -16,9 +16,8 @@ if(!isset($_SESSION['user_name'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>user page</title>
+   <title>User Page</title>
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -30,8 +29,8 @@ if(!isset($_SESSION['user_name'])){
       <h3>hi, <span>user</span></h3>
       <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
       <p>this is an user page</p>
-      <a href="login_form.php" class="btn">login</a>
-      <a href="register_form.php" class="btn">register</a>
+      <a href="index.php" class="btn">to-do list</a>
+      <!-- <a href="register_form.php" class="btn">register</a> -->
       <a href="logout.php" class="btn">logout</a>
    </div>
 
